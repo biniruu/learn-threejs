5973f9276f501627fbff39867b933e59f80c1d59
biniruu <biniruu@gmail.com>
2022-06-06 00:18:04 +0900

    feat: implement first cube animation

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-import './style.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
+camera.position.z = 5
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+document.getElementById('app').appendChild(renderer.domElement)
+renderer.setSize(innerWidth, innerHeight)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate() {
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+}
+animate()
